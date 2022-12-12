46c84a43d6f4c40da89732baf626cb9871fee102
Iliya Yanev <iliyayanev92@gmail.com>
2022-12-12 17:16:51 +0200

    movie model

---
+++ server/models/Movie.js
@@ -0,0 +1,18 @@
+const { model, Schema, Types: { ObjectId } } = require('mongoose');
+
+const schema = new Schema({
+    title: { type: String, required: [true, 'Movie title is required'] },
+    year: {
+        type: Number,
+        required: true,
+        min: [1900, 'Year must be between 1900 and 2023'],
+        max: [2023, 'Year must be between 1900 and 2023']
+    },
+    description: { type: String, required: true, minlength: [10, 'Description must be at least 10 characters long'] },
+    img: { type: String, required: true, match: [/^https?:\/\//, 'The image url should start with http:// or https://'] },
+    likes: [{ type: ObjectId, ref: 'User' }]
+});
+
+const Movie = model('Movie', schema);
+
+module.exports = Movie;
