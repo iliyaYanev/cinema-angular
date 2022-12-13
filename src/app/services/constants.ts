a661ecb4d6f6260844aafbe85f73d6d05d6d5235
Iliya Yanev <iliyayanev92@gmail.com>
2022-12-13 19:45:06 +0200

    constants

--- src/app/services/constants.ts
+++ src/app/services/constants.ts
@@ -1,1 +1,4 @@
-export const moviesUrl = 'https://api.themoviedb.org/3/movie/upcoming?api_key=f7fcf2cafe6ce85f8afdc9f4e910e370';
+const baseUrl: string = 'https://api.themoviedb.org';
+const apiKey : string = 'f7fcf2cafe6ce85f8afdc9f4e910e370';
+
+export const moviesUrl: string = `${baseUrl}/3/movie/upcoming?api_key=${apiKey}`;
