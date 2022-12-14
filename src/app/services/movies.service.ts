d7735db445d156854111bbbf427a892703a46960
Iliya Yanev <iliyayanev92@gmail.com>
2022-12-14 09:16:52 +0200

    use movie dto

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
-import { moviesUrl } from './constants';
+import { BASE_URL, API_KEY } from './constants';
+import { MovieDTO } from "../models/movie";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesService {
-  constructor(private http: HttpClient) { }
 
-  getMovies() {
-    return this.http.get(moviesUrl);
+    constructor(private http: HttpClient) { }
+
+    getMovies(type: string = 'upcoming') {
+    return this.http.get<MovieDTO>(`${BASE_URL}/3/movie/${type}?api_key=${API_KEY}`);
   }
 }
