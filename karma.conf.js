c2edb06aa3f91dbb330450b7dc29b67c97f1403c
Iliya Yanev <iliyayanev92@gmail.com>
2022-12-14 23:27:35 +0200

    update refs

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/movies-app'),
+      dir: require('path').join(__dirname, './coverage/soft-cinema'),
       subdir: '.',
       reporters: [
         { type: 'html' },
