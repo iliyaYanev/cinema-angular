3f568f4cd87500347eb9a23775f680581011ff50
Iliya Yanev <iliyayanev92@gmail.com>
2022-12-14 16:00:32 +0200

    movie comp

---
+++ src/app/pages/movie/movie.component.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit} from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { MoviesService } from "../../services/movies.service";
+import { Movie } from "../../models/movie";
+
+@Component({
+    selector: 'app-movie',
+    templateUrl: './movie.component.html',
+    styleUrls: ['./movie.component.scss']
+})
+export class MovieComponent implements OnInit {
+
+    movie: Movie | null = null;
+
+    constructor(private route: ActivatedRoute, private moviesService: MoviesService) {}
+
+    ngOnInit(): void {
+        this.route.params.subscribe(({ id }) => {
+            this.getMovie(id);
+        });
+    }
+
+    getMovie(id: string) {
+        this.moviesService.getMovie(id)
+            .subscribe(movieData => {
+                this.movie = movieData;
+            });
+    }
+}
