d7960b6286206b52c6e202221878f5bceb4cfd0a
Iliya Yanev <iliyayanev92@gmail.com>
2022-12-14 10:34:27 +0200

    tv show data

--- src/app/components/items-banner/items-banner.component.ts
+++ src/app/components/items-banner/items-banner.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Movie } from "../../models/movie";
-import {TvShow} from "../../models/tvShow";
+import { TvShow } from "../../models/tvShow";
 
 @Component({
   selector: 'app-items-banner',
