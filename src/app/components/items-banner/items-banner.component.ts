93c450407ab03419417e5f065ba5b22d806aabb2
Iliya Yanev <iliyayanev92@gmail.com>
2022-12-14 09:19:12 +0200

    items banner comp

---
+++ src/app/components/items-banner/items-banner.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { Movie } from "../../models/movie";
+
+@Component({
+  selector: 'app-items-banner',
+  templateUrl: './items-banner.component.html',
+  styleUrls: ['./items-banner.component.scss']
+})
+export class ItemsBannerComponent {
+
+    @Input() movies: Movie[] = [];
+    @Input() title: String = '';
+}
