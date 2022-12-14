db85300ec3ace38ccc5d5171494f7fc88593ba98
Iliya Yanev <iliyayanev92@gmail.com>
2022-12-14 09:17:44 +0200

    add image sizes

--- src/app/services/constants.ts
+++ src/app/services/constants.ts
@@ -1,4 +1,10 @@
-const baseUrl: string = 'https://api.themoviedb.org';
-const apiKey : string = 'f7fcf2cafe6ce85f8afdc9f4e910e370';
+export const BASE_URL: string = 'https://api.themoviedb.org';
+export const API_KEY : string = 'f7fcf2cafe6ce85f8afdc9f4e910e370';
 
-export const moviesUrl: string = `${baseUrl}/3/movie/upcoming?api_key=${apiKey}`;
+export const IMAGE_BASE_URL: string = 'https://image.tmdb.org/t/p';
+
+export const IMAGE_SIZES = {
+    small: `${IMAGE_BASE_URL}/w185`,
+    medium: `${IMAGE_BASE_URL}/w342`,
+    large: `${IMAGE_BASE_URL}/original`
+}
