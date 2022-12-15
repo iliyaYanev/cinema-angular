69c8d319c50baf8c64b7f96498116d1084fbb965
Iliya Yanev <iliyayanev92@gmail.com>
2022-12-16 00:24:00 +0200

    update register component

--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -4,6 +4,7 @@ import { CustomValidators } from '../../../constants/custom-validator';
 import { AuthService } from '../../../services/auth.service';
 import { tap } from 'rxjs';
 import { Router } from '@angular/router';
+import { RegisterRequest } from "../../../models/auth";
 
 @Component({
   selector: 'app-register',
@@ -11,30 +12,26 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+    registerForm = new FormGroup({
+            email: new FormControl(null, [Validators.required, Validators.email]),
+            username: new FormControl(null, [Validators.required]),
+            firstName: new FormControl(null, [Validators.required]),
+            lastName: new FormControl(null, [Validators.required]),
+            password: new FormControl(null, [Validators.required]),
+            passwordConfirm: new FormControl(null, [Validators.required])
+        },
+        {validators: CustomValidators.passwordsMatching}
+    )
 
-  registerForm = new FormGroup({
-        email: new FormControl(null, [Validators.required, Validators.email]),
-        username: new FormControl(null, [Validators.required]),
-        firstname: new FormControl(null, [Validators.required]),
-        lastname: new FormControl(null, [Validators.required]),
-        password: new FormControl(null, [Validators.required]),
-        passwordConfirm: new FormControl(null, [Validators.required])
-      },
-      { validators: CustomValidators.passwordsMatching }
-  )
-
-  constructor(
-      private router: Router,
-      private authService: AuthService
-  ) { }
-
-  register() {
-    if (!this.registerForm.valid) {
-      return;
+    constructor(private router: Router, private authService: AuthService) {
     }
-    this.authService.register()
-        .pipe(tap(() => this.router.navigate(['../auth/login'])))
-        .subscribe();
-  }
 
+    register() {
+        if (!this.registerForm.valid) {
+            return;
+        }
+        this.authService.register(<RegisterRequest><unknown>this.registerForm.value)
+            .pipe(tap(() => this.router.navigate(['../'])))
+            .subscribe();
+    }
 }
