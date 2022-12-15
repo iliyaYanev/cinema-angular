7b9a82e259e59287e9929bc8dfef043df0df0774
Iliya Yanev <iliyayanev92@gmail.com>
2022-12-15 22:43:04 +0200

    auth const

--- src/app/constants/auth-constants.ts
+++ src/app/constants/auth-constants.ts
@@ -1,24 +1,8 @@
-import { LoginResponse, RegisterResponse } from "../models/auth";
+export const LOCALSTORAGE_TOKEN_KEY = 'auth_token';
 
-export const fakeLoginResponse: LoginResponse = {
-    // mock
-    accessToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
-    refreshToken: {
-        id: 1,
-        userId: 2,
-        token: 'fakeRefreshToken...should al come from real backend',
-        refreshCount: 2,
-        expiryDate: new Date(),
-    },
-    tokenType: 'JWT'
-}
-
-export const fakeRegisterResponse: RegisterResponse = {
-    status: 200,
-    message: 'Registration was successful.'
-}
-
-export const LOCALSTORAGE_TOKEN_KEY = 'mock_token';
+export const BASE_URL = 'http://localhost:3030';
+export const LOGIN_PATH = BASE_URL + '/auth/login';
+export const REGISTER_PATH = BASE_URL + '/auth/register';
 
 export function tokenGetter() {
     return localStorage.getItem(LOCALSTORAGE_TOKEN_KEY);
