190ee806d68b5f88ae586cbc50193dba3d212359
Iliya Yanev <iliyayanev92@gmail.com>
2022-12-15 16:30:25 +0200

    add guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,8 @@ import { MoviesComponent } from './pages/movies/movies.component';
 import { TvShowsComponent } from './pages/tv-shows/tv-shows.component';
 import { TvShowComponent } from './pages/tv-show/tvshow.component';
 import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from "./components/auth/register/register.component";
+import { AuthGuard } from "./services/auth-guard/auth-guard";
 
 const routes: Routes = [
   {
@@ -18,32 +20,43 @@ const routes: Routes = [
     component: LoginComponent
   },
   {
+    path: 'auth/register',
+    component: RegisterComponent
+  },
+  {
     path: 'movies',
-    component: MoviesComponent
+    component: MoviesComponent,
+    canActivate: [AuthGuard]
   },
   {
     path: 'movies/genres/:genreId',
-    component: MoviesComponent
+    component: MoviesComponent,
+    canActivate: [AuthGuard]
   },
   {
     path: 'movie/:id',
-    component: MovieComponent
+    component: MovieComponent,
+    canActivate: [AuthGuard]
   },
   {
     path: 'tvshows',
-    component: TvShowsComponent
+    component: TvShowsComponent,
+    canActivate: [AuthGuard]
   },
   {
     path: 'tv-show/:id',
-    component: TvShowComponent
+    component: TvShowComponent,
+    canActivate: [AuthGuard]
   },
   {
     path: 'tvshows/genres/:genreId',
-    component: TvShowsComponent
+    component: TvShowsComponent,
+    canActivate: [AuthGuard]
   },
   {
     path: 'genres',
-    component: GenresComponent
+    component: GenresComponent,
+    canActivate: [AuthGuard]
   },
   {
     path: '**',
