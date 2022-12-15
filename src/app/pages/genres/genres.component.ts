f8fa1425e339be193f7b6b05fc818357831bced0
Iliya Yanev <iliyayanev92@gmail.com>
2022-12-15 22:38:42 +0200

    genres animation

--- src/app/pages/genres/genres.component.ts
+++ src/app/pages/genres/genres.component.ts
@@ -2,11 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { MoviesService } from 'src/app/services/movies.service';
 import { TvShowsService } from 'src/app/services/tvshows.service';
 import { Genre } from '../../models/genre';
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-genres',
   templateUrl: './genres.component.html',
-  styleUrls: ['./genres.component.scss']
+  styleUrls: ['./genres.component.scss'],
+  animations: [
+    trigger('slideFade', [
+      state('void', style({ opacity: 0 })),
+      transition('void <=> *', [animate('1s')])
+    ])
+  ]
 })
 export class GenresComponent implements OnInit {
   genres: Genre[] = [];
