2f78dd8dd48fa7fdbc301759b3fd393388af7ebd
Iliya Yanev <iliyayanev92@gmail.com>
2022-12-16 15:02:24 +0200

    items banner component spec

--- src/app/components/items-banner/items-banner.component.spec.ts
+++ src/app/components/items-banner/items-banner.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ItemsBannerComponent } from './items-banner.component';
+import { RouterModule } from "@angular/router";
+import { HttpClientModule } from "@angular/common/http";
 
 describe('ItemsBannerComponent', () => {
   let component: ItemsBannerComponent;
@@ -8,7 +10,8 @@ describe('ItemsBannerComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ItemsBannerComponent ]
+      declarations: [ ItemsBannerComponent ],
+      imports: [ RouterModule.forRoot([]), HttpClientModule ]
     })
     .compileComponents();
   });
