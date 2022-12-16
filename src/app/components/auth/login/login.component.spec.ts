b94e49a1b0cc4b72dac8458a8e6d12e7e81dab26
Iliya Yanev <iliyayanev92@gmail.com>
2022-12-16 15:04:15 +0200

    login component spec

--- src/app/components/auth/login/login.component.spec.ts
+++ src/app/components/auth/login/login.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { RouterModule } from "@angular/router";
+import { HttpClientModule } from "@angular/common/http";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,7 +11,8 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      imports: [ RouterModule.forRoot([]), HttpClientModule, MatSnackBarModule ]
     })
     .compileComponents();
 
