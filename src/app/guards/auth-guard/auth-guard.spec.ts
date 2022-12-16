b5e063a070f68be141cf83f68b0f088fc2ac32d3
Iliya Yanev <iliyayanev92@gmail.com>
2022-12-16 15:00:39 +0200

    auth guard spec

--- src/app/guards/auth-guard/auth-guard.spec.ts
+++ src/app/guards/auth-guard/auth-guard.spec.ts
@@ -1,12 +1,23 @@
 import { TestBed } from '@angular/core/testing';
 
-import { AuthGuard } from './auth-guard';
+import { HttpClientModule } from "@angular/common/http";
+import { AuthGuard } from "./auth-guard";
+import { JwtModule } from "@auth0/angular-jwt";
+import { tokenGetter } from "../../constants/auth-constants";
 
-describe('AuthGuardService', () => {
+describe('AuthGuard', () => {
   let service: AuthGuard;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ HttpClientModule, JwtModule.forRoot({
+        config: {
+          tokenGetter: tokenGetter,
+          allowedDomains: ['localhost:3000', 'localhost:8080']
+        }})
+      ]
+    });
+
     service = TestBed.inject(AuthGuard);
   });
 
