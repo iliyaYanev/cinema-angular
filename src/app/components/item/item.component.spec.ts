50d4b36cd29b4b826e9d5cd865db55ac1eaa9d7c
Iliya Yanev <iliyayanev92@gmail.com>
2022-12-16 15:03:21 +0200

    item component spec

--- src/app/components/item/item.component.spec.ts
+++ src/app/components/item/item.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ItemComponent } from './item.component';
+import { RouterModule } from "@angular/router";
+import { HttpClientModule } from "@angular/common/http";
 
 describe('MovieItemComponent', () => {
   let component: ItemComponent;
@@ -8,7 +10,8 @@ describe('MovieItemComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ItemComponent ]
+      declarations: [ ItemComponent ],
+      imports: [ RouterModule.forRoot([]), HttpClientModule ]
     })
     .compileComponents();
   });
